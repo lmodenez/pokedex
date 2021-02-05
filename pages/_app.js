0a42a9992ae28265a6fc5bf5af5e5d2832f9516d
lmodenez <lmodenez98@gmail.com>
2021-02-05 19:54:55 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
